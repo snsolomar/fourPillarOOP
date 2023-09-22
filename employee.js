b932c92fa858021b2f0e8d37e36606f2395833fa
snsolomar <snsolomar@gmail.com>
2023-09-21 21:45:19 -0700

    first commit

---
+++ employee.js
@@ -0,0 +1,39 @@
+// Encapsulation
+class Employee {
+    constructor(wage, hoursWorked){
+        this.wage = wage;
+        this.hoursWorked = hoursWorked;
+    }
+
+    getPay(){
+        return this.wage * this.hoursWorked;
+    }
+}
+
+// Inheritance
+class Manager extends Employee {
+    constructor(wage, hoursWorked, bonus){
+        super(wage, hoursWorked);
+        this.bonus = bonus;
+    }
+
+    getPay(){
+        return super.getPay() + this.bonus;
+    }
+}
+
+// Abstraction
+
+const manager = new Manager(20, 40, 200);
+console.log("manager makes:", manager.getPay()); // You don't need to know how getPay() works, just that it does
+
+// Polymorphism
+
+function calculateTotalPay(employee){
+    return employee.getPay();
+}
+
+const regularEmployee = new Employee(15,40);
+console.log("regular employee makes:", calculateTotalPay(regularEmployee));
+
+
